a12f17e172e5a9f9c277dac752e5a4d12f839733
ravikiran991 <107342146+ravikiran991@users.noreply.github.com>
2022-07-16 10:25:57 +0530

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,22 @@
+let colorPickerContainer = document.getElementById("colorPickerContainer");
+let selectedColorHexCode = document.getElementById("selectedColorHexCode");
+
+function colorOne() {
+    colorPickerContainer.style.backgroundColor = "#e0e0e0";
+    selectedColorHexCode.textContent = "#e0e0e0";
+}
+
+function colortwo() {
+    colorPickerContainer.style.backgroundColor = "#6fcf97";
+    selectedColorHexCode.textContent = "#6fcf97";
+}
+
+function colorthree() {
+    colorPickerContainer.style.backgroundColor = "#56ccf2";
+    selectedColorHexCode.textContent = "#56ccf2";
+}
+
+function colorfour() {
+    colorPickerContainer.style.backgroundColor = "#bb6bd9";
+    selectedColorHexCode.textContent = "#bb6bd9";
+}
